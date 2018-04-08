237eadd7e8eef7d7ca18e8bf8ff2d4af2485d7ab
Fleodon <36356299+Fleodon@users.noreply.github.com>
2018-04-08 16:07:05 +0200

    Update init.lua

--- f_transportsystem/lua/entities/fts_taxi/init.lua
+++ f_transportsystem/lua/entities/fts_taxi/init.lua
@@ -17,14 +17,17 @@ end
 
 function ENT:AcceptInput( Name, Activator, Caller )	
 	if Name == "Use" && IsValid( Caller ) && Caller:IsPlayer() then
-		if (!self.nextUse or CurTime() >= self.nextUse) then
-			if file.Read( "ftransportsystem/admin/taxivariables.txt", "DATA") == FTransportSystem.Language[FTS_BaseLang].Value1 then 
-				net.Start( "FHS:Player:OpenMenu" )
-				net.Send( Caller )
-				self.nextUse = CurTime() + 1	
-			else
-				DarkRP.notify(Caller, 1, 5, FTransportSystem.Language[FTS_BaseLang].DisabledNotify) 
-			end 
+		if Caller:GetPos():DistToSqr(self:GetPos())<200 then
+			if (!self.nextUse or CurTime() >= self.nextUse) then
+				if file.Read( "ftransportsystem/admin/taxivariables.txt", "DATA") == FTransportSystem.Language[FTS_BaseLang].Value1 then 
+					net.Start( "FHS:Player:OpenMenu" )
+						net.WriteEntity( self )
+					net.Send( Caller )
+					self.nextUse = CurTime() + 1	
+				else
+					DarkRP.notify(Caller, 1, 5, FTransportSystem.Language[FTS_BaseLang].DisabledNotify) 
+				end 
+			end
 		end
-	end
+	end 
 end
